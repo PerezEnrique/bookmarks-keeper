a6b533d4bff5e29b5497eab6fc9558fd39267362
Enrique Perez <28enriqueperez@gmail.com>
2021-10-19 06:01:07 -0400

    fix: fix problem with token check happening too late in auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,8 +4,8 @@ const { jwtPrivateKey } = require("../config/app-config");
 
 module.exports = function (req, res, next) {
 	const headerData = req.header("Authorization");
+	if (!headerData) throw boom.unauthorized();
 	const token = headerData.replace("Bearer", "").trim();
-	if (!token) throw boom.unauthorized();
 
 	//if the token is not valid, jwt.verify() will throw an exception, that's why we need a try catch block
 	try {
