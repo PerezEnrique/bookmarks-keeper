1ae6defd1d9a997c7e6e7322228d95569c5e420f
Enrique Perez <28enriqueperez@gmail.com>
2021-10-25 08:36:16 -0400

    feat: finish tests for users endpoints

--- src/__test__/integration/users.test.js
+++ src/__test__/integration/users.test.js
@@ -12,6 +12,11 @@ afterEach(async () => {
 	server.close();
 });
 
+const testUserData = {
+	username: "testUsername",
+	password: "testPassword",
+};
+
 describe("POST /users/", () => {
 	function requestCreateUser(username, password) {
 		return request(server).post("/users/").send({ username, password });
@@ -27,7 +32,7 @@ describe("POST /users/", () => {
 		expect(response.status).toBe(400);
 	});
 
-	it("should return a 400 error if username is more than 30 characters", async () => {
+	it("should return a 400 error if username is equal to or more than 30 characters", async () => {
 		const response = await requestCreateUser(new Array(32).join("a"), "testPassword");
 		expect(response.status).toBe(400);
 	});
@@ -37,7 +42,7 @@ describe("POST /users/", () => {
 		expect(response.status).toBe(400);
 	});
 
-	it("should return a 400 error if password is more than 1024 characters", async () => {
+	it("should return a 400 error if password is equal to or more than 1024 characters", async () => {
 		const response = await requestCreateUser("testUsername", new Array(1026).join("a"));
 		expect(response.status).toBe(400);
 	});
@@ -52,13 +57,145 @@ describe("POST /users/", () => {
 		expect(response.body.error).toMatch(/An user with provided username already exists/i);
 	});
 
-	it("should save the user, if it is valid", async () => {
+	it("should save the user, and return an status code 200, if it is valid", async () => {
 		const testUsername = "testUsername";
 		//make request with valid data to create a new user
-		await requestCreateUser(testUsername, "testPassword");
+		const response = await requestCreateUser(testUsername, "testPassword");
+		expect(response.status).toBe(201);
 
-		//then we query that user in the db
+		//query that user in db
 		const createdUser = await User.find({ username: testUsername });
-		expect(createdUser).not.toBeNull();
+		expect(createdUser.length).toBe(1);
+	});
+});
+
+describe("PUT /users/:id", () => {
+	function requestUpdateUser(id, token, username, password) {
+		return request(server)
+			.put(`/users/${id}`)
+			.set("Authorization", `Bearer ${token}`)
+			.send({ username, password });
+	}
+
+	it("should return a 401 error if user is not logged in", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		await testUser.save();
+
+		//send request without token
+		const response = await requestUpdateUser(testUser.id, "", "", "");
+		expect(response.status).toBe(401);
+	});
+
+	it("should return a 400 error if user leaves both fields empty", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+
+		//send request with both fields empty
+		const response = await requestUpdateUser(testUser.id, token, "", "");
+		expect(response.status).toBe(400);
+	});
+
+	it("should return a 400 error if username is equal to or more than 30 characters", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+
+		//send request with 30 characters username
+		const response = await requestUpdateUser(
+			testUser.id,
+			token,
+			new Array(32).join("a"),
+			""
+		);
+
+		expect(response.status).toBe(400);
+	});
+
+	it("should return a 400 error if password is less than 5 characters", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+
+		//send request with only 4 characters password
+		const response = await requestUpdateUser(testUser.id, token, "", "1234");
+
+		expect(response.status).toBe(400);
+	});
+
+	it("should return a 400 error if password is equal to or more than 1024 characters", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+
+		//send request with 1024 characters password
+		const response = await requestUpdateUser(
+			testUser.id,
+			token,
+			"",
+			new Array(1026).join("a")
+		);
+
+		expect(response.status).toBe(400);
+	});
+
+	it("should return, if unexisting id is provided, a 404 error with the message 'Cound't find user with provided id'", async () => {
+		// //create the user we later will edit but we don't save it
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		const newUsername = "testUser2";
+
+		//user was not saved on db so it shouldn't find it when we request
+		const response = await requestUpdateUser(testUser.id, token, newUsername, "");
+		expect(response.status).toBe(404);
+		expect(response.body.error).toMatch(/Cound't find user with provided id/i);
+	});
+
+	it("should update the user, and return an status code 200, if it is valid", async () => {
+		// //create the user we later will edit
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+		const newUsername = "testUser2";
+
+		//send update request with valid data
+		const response = await requestUpdateUser(testUser.id, token, newUsername, "");
+		expect(response.status).toBe(201);
+
+		//query the updated user in db
+		const updatedUser = await User.find({ username: newUsername });
+		expect(updatedUser.length).toBe(1);
+	});
+});
+
+describe("DELETE /users/:id", () => {
+	function requestDeleteUser(id, token) {
+		return request(server).delete(`/users/${id}`).set("Authorization", `Bearer ${token}`);
+	}
+
+	it("should return a 401 error if user is not logged in", async () => {
+		//create user that we later will delete
+		const testUser = new User(testUserData);
+		await testUser.save();
+
+		//send request with no token
+		const response = await requestDeleteUser(testUser.id, "");
+		expect(response.status).toBe(401);
+	});
+
+	it("should delete the user, and return an status code 200", async () => {
+		//create user that we later will delete
+		const testUser = new User(testUserData);
+		const token = testUser.generateAuthToken();
+		await testUser.save();
+
+		//send request
+		const response = await requestDeleteUser(testUser.id, token);
+		expect(response.status).toBe(200);
 	});
 });
