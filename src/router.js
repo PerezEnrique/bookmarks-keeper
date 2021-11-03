68d106edb84af52e3add09af6c3e934d15f7fb21
Enrique Perez <28enriqueperez@gmail.com>
2021-11-03 08:12:59 -0400

    refactor: Add prefix api to routes

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-const auth = require("./controllers/auth-controller");
-const users = require("./controllers/users-controller");
-const bookmarks = require("./controllers/bookmarks-controller");
+const auth = require('./controllers/auth-controller');
+const users = require('./controllers/users-controller');
+const bookmarks = require('./controllers/bookmarks-controller');
 
 module.exports = function (app) {
-	app.use("/auth", auth);
-	app.use("/users", users);
-	app.use("/bookmarks", bookmarks);
+	app.use('/api/auth', auth);
+	app.use('/api/users', users);
+	app.use('/api/bookmarks', bookmarks);
 };
