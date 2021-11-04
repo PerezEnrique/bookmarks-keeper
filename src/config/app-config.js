e789a66d122b1d5978a7b9b99057d83a64d261b5
Enrique Perez <28enriqueperez@gmail.com>
2021-11-04 05:10:07 -0400

    feat: Enable cors for client

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 module.exports = {
 	nodeEnv: process.env.NODE_ENV,
+	clientOrigin: process.env.CLIENT_ORIGIN,
 	port: process.env.PORT || 5000,
 	dbUri: process.env.NODE_ENV === "test" ? process.env.TEST_DB_URI : process.env.DB_URI,
 	jwtPrivateKey: process.env.JWT_PRIVATE_KEY,
