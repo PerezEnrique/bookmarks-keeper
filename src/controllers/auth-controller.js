0355098375925975b9cbe15dae760df32bfd5738
Enrique Perez <28enriqueperez@gmail.com>
2021-11-04 05:10:38 -0400

    fix: Add validations to login endpoint

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
 const passport = require("passport");
+const validation = require("../middlewares/validation-handler");
+const { createUser } = require("../utils/validation-schemas/users-validation-schemas");
 
 //full path: /auth/login
 //method: post
 //desc: authenticates user
 
-router.post("/login", passport.authenticate("local", { session: false }), (req, res) => {
-	const { user } = req;
-	const token = user.generateAuthToken();
-	const userToReturn = {
-		_id: user._id,
-		username: user.username,
-		bookmarks: user.bookmarks,
-	};
-	res.header("Authorization", `Bearer ${token}`).json(userToReturn);
-});
+router.post(
+	"/login",
+	validation(createUser),
+	passport.authenticate("local", { session: false }),
+	(req, res) => {
+		const { user } = req;
+		const token = user.generateAuthToken();
+		const userToReturn = {
+			_id: user._id,
+			username: user.username,
+			bookmarks: user.bookmarks,
+		};
+		res.header("Authorization", `Bearer ${token}`).json(userToReturn);
+	}
+);
 
 module.exports = router;
